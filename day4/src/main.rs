0ae1d2ca824d894bf5f63d50c04d877cc6f5b823
jthro <jethro.rosette@outlook.com>
2024-12-25 22:09:09 +1100

    day 4 works now

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -18,30 +18,32 @@ fn main() {
 
     let mut result: usize = 0;
 
-    for row in resultu8.windows(4) {
-
-        for (j, _) in row[0].iter().enumerate() {
-
-            if j < row[0].len() - 3 {
-                let rowmas: String = String::from_utf8(vec!(row[0][j], row[0][j+1], row[0][j+2], row[0][j+3])).unwrap();
-                if rowmas == "XMAS" || rowmas == "SAMX" {
+    for w in resultu8.windows(4) {
+        for (i, _) in w[0].iter().enumerate() {
+            if i < w[0].len() - 3 {
+                let diag = String::from_utf8(vec!(w[0][i], w[1][i+1], w[2][i+2], w[3][i+3])).unwrap();
+                if diag == "XMAS" || diag == "SAMX" {
                     result += 1;
                 }
 
-                let diag1: String = String::from_utf8(vec!(row[0][j], row[1][j+1], row[2][j+2], row[3][j+3])).unwrap();
-                let diag2: String = String::from_utf8(vec!(row[0][j + 3], row[1][j+2], row[2][j+1], row[3][j])).unwrap();
+                let diag = String::from_utf8(vec!(w[3][i], w[2][i+1], w[1][i+2], w[0][i+3])).unwrap();
+                if diag == "XMAS" || diag == "SAMX" {
+                    result += 1;
+                }
 
-                if diag1 == "XMAS" || diag1 == "SAMX" {
-                    result += 1;
-                }
-                if diag2 == "XMAS" || diag2 == "SAMX" {
-                    result += 1;
-                }
             }
 
-            let colmas: String = String::from_utf8(vec!(row[0][j], row[1][j], row[2][j], row[3][j])).unwrap();
+            let vertical = String::from_utf8(vec!(w[0][i], w[1][i], w[2][i], w[3][i])).unwrap();
+            if vertical == "XMAS" || vertical == "SAMX" {
+                result += 1;
+            }
+        }
+    }
 
-            if colmas == "XMAS" || colmas == "SAMX" {
+    for row in resultu8.iter() {
+        for w in row.windows(4) {
+            let horizontal = String::from_utf8(w.to_vec()).unwrap();
+            if horizontal == "XMAS" || horizontal == "SAMX" {
                 result += 1;
             }
         }
